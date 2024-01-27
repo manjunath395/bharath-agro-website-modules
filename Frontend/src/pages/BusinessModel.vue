4e3f2837faa6a9b26edaac3db0b79bfd3fc37cd7
manjunathmi8147@gmail.com <=>
2024-01-27 18:19:10 +0530

    initial commit

---
+++ Frontend/src/pages/BusinessModel.vue
@@ -0,0 +1,47 @@
+<template>
+      <main id="content" class="main main-page">
+            <div id="primary" class="main-page-primary">
+                <article id="post-37" class="article-page hentry">
+                    <header class="entry-header article-page-header "
+                        style="background-image: url('wp-content/themes/npc/images/bg_page.jpg');">
+                        <div class="home-slideshow owl-carousel owl-theme">
+                            <div class="home-slide" style="background-image: url('wp-content/themes/npc/images/products/image1.jpg');">
+                                <a>
+                                    
+                                </a> </div>
+                            <div class="home-slide" style="background-image: url('wp-content/themes/npc/images/products/image2.jpg'); height:80%; width:80%;">
+                                <a> </a> </div>
+                            <div class="home-slide" style="background-image: url('wp-content/themes/npc/images/products/image3.jpg');">
+                                <a> </a> </div>
+                             <div class="home-slide" style="background-image: url('wp-content/themes/npc/images/products/image4.jpg');">
+                                <a> </a> </div>
+                             <div class="home-slide" style="background-image: url('wp-content/themes/npc/images/products/image6.jpg');">
+                                <a> </a> </div>     
+                             <div class="home-slide" style="background-image: url('wp-content/themes/npc/images/products/image7.jpg');">
+                                <a> </a> </div>       
+                        </div>
+                    </header>
+                    <div class="entry-content article-page-content" itemprop="mainContentOfPage">
+                        <div class="wrapper">
+                            <div class="vc_row wpb_row vc_row-fluid">
+                                <div class="wpb_column vc_column_container vc_col-sm-12">
+                                    <div class="vc_column-inner">
+                                        <div class="wpb_wrapper">
+                                            <div class="wpb_text_column wpb_content_element ">
+                                                <div class="wpb_wrapper">
+                                                    
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <footer class="entry-meta article-page-footer">
+                        <div class="wrapper"> </div>
+                    </footer>
+                </article>
+            </div>
+        </main>
+</template>
